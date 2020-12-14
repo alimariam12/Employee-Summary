a30ee051bb0502f5555096832fbf6aa609d913ab
Mariam Ali <alim12@csp.edu>
2020-12-14 17:16:53 -0600

    when it to generate

--- Develop/app.js
+++ Develop/app.js
@@ -6,41 +6,87 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputPath = path.join(OUTPUT_DIR, "index.html");
 
 const render = require("./lib/htmlRenderer");
 let Questions = [];
 
 function generateTeam() {
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "What is your name?",
-      name: "Name",
-    },
-    {
-      type: "input",
-      message: "What is your team ID?",
-      name: "ID",
-    },
-    {
-      type: "input",
-      message: "What is your email?",
-      name: "Email",
-    },
-    {
-      type: "list",
-      message: "What is your role?",
-      name: "Role",
-      options: ["Intern", "Manager", "Engineer"],
-    },
-  ])
-  .then((function({name, role, id, email}) {
-      
-  
-
-  })
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "Name",
+      },
+      {
+        type: "input",
+        message: "What is your team ID?",
+        name: "ID",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "Email",
+      },
+      {
+        type: "list",
+        message: "What is your role?",
+        name: "Role",
+        options: ["Intern", "Manager", "Engineer"],
+      },
+    ])
+    .then(function ({ name, role, id, email }) {
+      let roleQuestions = [];
+      if (role === "Engineer") {
+        roleQuestions = "Enter your GitHub username please";
+      } else if (role === "Intern") {
+        roleQuestions = "Enter your school please";
+      } else {
+        roleQuestions = "Add an office number";
+      }
+      inquirer
+        .prompt([
+          {
+            message: `What is this member's ${roleQuestions}?`,
+            name: "roleQuestions",
+          },
+          {
+            type: "list",
+            message: "Are there any more team members you would like too add?",
+            name: "addTeamMembers",
+            options: ["yes", "no"],
+          },
+        ])
+        .then(function ({ roleQuestions, newMembers }) {
+          let teamMembers;
+          if (role === "Engineer") {
+            teamMembers = new Engineer(name, id, email, roleQuestions);
+          } else if (role === "Intern") {
+            teamMembers = new Intern(name, id, email, roleQuestions);
+          } else {
+            teamMembers = new Manager(name, id, email, roleQuestions);
+          }
+          employees.push(teamMembers);
+          if (addMembers) {
+            generateTeam();
+          } else {
+            render(employees);
+            fs.writeFile(outputPath, render(employees), (err) => {
+              if (err) {
+                throw err;
+              }
+            });
+          }
+        })
+        .catch((err) => {
+          if (err) {
+          }
+        });
+    });
 }
+
+generateTeam();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
